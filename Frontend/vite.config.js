0548951184d5c58744262504769d991a54033bb6
Ankush Raj <157215485+112ankushraj@users.noreply.github.com>
2025-07-20 10:49:57 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://code-reviewer-5gik.onrender.com',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 });
